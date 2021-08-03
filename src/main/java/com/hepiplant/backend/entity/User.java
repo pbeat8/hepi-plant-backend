6a0561aab412e1927f19a45b537e8c6448955b8c
Patrycja Zmilczak <patiz@vp.pl>
2021-08-03 19:28:17 +0200

    refactor user

--- src/main/java/com/hepiplant/backend/entity/User.java
+++ src/main/java/com/hepiplant/backend/entity/User.java
@@ -12,6 +12,67 @@ public class User {
     private String login;
     private String password;
     private String email;
-    @Enumerated(EnumType.String)
+    @Enumerated(EnumType.STRING)
     private Permission permission;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String login, String password, String email, Permission permission) {
+        this.id = id;
+        this.username = username;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+        this.permission = permission;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Permission getPermission() {
+        return permission;
+    }
+
+    public void setPermission(Permission permission) {
+        this.permission = permission;
+    }
+
 }
