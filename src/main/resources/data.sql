fa01f30e6b462f81e79b57e12d28e644a9d75fc7
Patrycja Zmilczak <patiz@vp.pl>
2021-08-05 19:21:14 +0200

    add into data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,11 @@
-INSERT INTO plants.categories(id, name)
-	VALUES (?, ?);
+INSERT INTO plants.categories(id, name) VALUES
+	(101, 'Rośliny ozdobne z liści'),
+	(102, 'Rośliny ozdobne z kwiatów'),
+	(103, 'Rośliny pnące i zwisające'),
+	(104, 'Oplątwy'),
+	(105, 'Sukulenty'),
+	(106, 'Kaktusy');
 
-INSERT INTO plants.species(id, fertilizing_frequency, misting_frequency, name, placement, soil, watering_frequency, category_id)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+INSERT INTO plants.species(id, fertilizing_frequency, misting_frequency, name, placement, soil, watering_frequency, category_id) VALUES
+    (201, ?, ?, ?, ?, ?, ?, ?),
+    (202, ?, ?, ?, ?, ?, ?, ?);
