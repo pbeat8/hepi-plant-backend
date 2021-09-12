b62e8934213cb2d867e9d3fb325f23e1aede347d
Aleksandra-10 <56477762+Aleksandra-10@users.noreply.github.com>
2021-09-12 16:39:44 +0200

    Category and Event, Plant

--- src/test/java/com/hepiplant/backend/service/impl/PlantServiceImplTest.java
+++ src/test/java/com/hepiplant/backend/service/impl/PlantServiceImplTest.java
@@ -3,6 +3,7 @@ package com.hepiplant.backend.service.impl;
 import com.hepiplant.backend.dto.PlantDto;
 import com.hepiplant.backend.entity.*;
 import com.hepiplant.backend.entity.enums.Permission;
+import com.hepiplant.backend.exception.InvalidBeanException;
 import com.hepiplant.backend.repository.*;
 import com.hepiplant.backend.validator.BeanValidator;
 import org.junit.jupiter.api.BeforeAll;
@@ -17,11 +18,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static com.hepiplant.backend.entity.enums.Placement.BARDZO_JASNE;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
@@ -29,8 +30,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.atMostOnce;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PlantServiceImplTest {
@@ -85,7 +85,6 @@ public class PlantServiceImplTest {
     @Test
     public void shouldCreatePlantOk(){
         //given
-        given(categoryRepository.findById(dto.getCategoryId())).willReturn(Optional.of(category));
         given(speciesRepository.findById(dto.getSpeciesId())).willReturn(Optional.of(species));
         given(userRepository.findById(dto.getUserId())).willReturn(Optional.of(user));
         given(plantRepository.save(plantArgumentCaptor.capture())).willAnswer(returnsFirstArg());
@@ -94,7 +93,6 @@ public class PlantServiceImplTest {
         PlantDto result = plantService.create(dto);
 
         //then
-        then(categoryRepository).should(times(1)).findById(eq(dto.getCategoryId()));
         then(speciesRepository).should(times(1)).findById(eq(dto.getSpeciesId()));
         then(userRepository).should(times(1)).findById(eq(dto.getUserId()));
         then(beanValidator).should(times(1)).validate(any());
@@ -102,9 +100,9 @@ public class PlantServiceImplTest {
         assertEquals(plant.getName(), result.getName());
         assertEquals(plant.getPurchaseDate(), result.getPurchaseDate());
         assertEquals(plant.getLocation(), result.getLocation());
-        assertEquals(plant.getCategory().getId(), result.getCategoryId());
-        assertEquals(plant.getSpecies().getId(), result.getSpeciesId());
-        assertEquals(plant.getUser().getId(), result.getUserId());
+        assertEquals(category.getId(), result.getCategoryId());
+        assertEquals(species.getId(), result.getSpeciesId());
+        assertEquals(user.getId(), result.getUserId());
 
         Plant captorValue = plantArgumentCaptor.getValue();
         assertEquals(plant.getName(), captorValue.getName());
@@ -116,17 +114,107 @@ public class PlantServiceImplTest {
 
     }
     @Test
-    public void shouldCreatePlantCategoryDoesNotExistThrowsException(){
+    public void shouldCreateUserDoesNotExistThrowsException(){
         //given
-        given(categoryRepository.findById(dto.getCategoryId())).willReturn(Optional.empty());
 
         //when
 
         //then
         assertThrows(EntityNotFoundException.class, () -> plantService.create(dto));
-        then(categoryRepository).should(times(1)).findById(eq(dto.getCategoryId()));
-        then(userRepository).should(atMostOnce()).findById(eq(dto.getUserId()));
         then(speciesRepository).should(atMostOnce()).findById(eq(dto.getSpeciesId()));
         then(plantRepository).should(times(0)).save(any(Plant.class));
+    }
+    @Test
+    public void shouldCreateSpeciesDoesNotExistThrowsException(){
+        //given
+
+        //when
+
+        //then
+        assertThrows(EntityNotFoundException.class, () -> plantService.create(dto));
+        then(userRepository).should(atMostOnce()).findById(eq(dto.getUserId()));
+        then(plantRepository).should(times(0)).save(any(Plant.class));
+    }
+    @Test
+    public void shouldCreatePlantInvalidValuesThrowsException() {
+        //given
+        given(speciesRepository.findById(dto.getSpeciesId())).willReturn(Optional.of(species));
+        given(userRepository.findById(dto.getUserId())).willReturn(Optional.of(user));
+        doThrow(InvalidBeanException.class).when(beanValidator).validate(any());
+
+        //when
+
+        //then
+        assertThrows(InvalidBeanException.class, () -> plantService.create(dto));
+        then(userRepository).should(times(1)).findById(eq(dto.getUserId()));
+        then(speciesRepository).should(times(1)).findById(eq(dto.getSpeciesId()));
+        then(beanValidator).should(times(1)).validate(any());
+        then(plantRepository).should(times(0)).save(any(Plant.class));
+    }
+
+    // GET ALL tests
+
+    @Test
+    public void shouldGetAllPlantsOk(){
+        //given
+        given(plantRepository.findAll()).willReturn(List.of(plant));
+
+        //when
+        List<PlantDto> result = plantService.getAll();
+
+        //then
+        then(plantRepository).should(times(1)).findAll();
+        assertEquals(1, result.size());
+        assertEquals(plant.getName(), result.get(0).getName());
+        assertEquals(plant.getPurchaseDate(), result.get(0).getPurchaseDate());
+        assertEquals(plant.getLocation(), result.get(0).getLocation());
+        assertEquals(category.getId(), result.get(0).getCategoryId());
+        assertEquals(species.getId(), result.get(0).getSpeciesId());
+        assertEquals(user.getId(), result.get(0).getUserId());
+
+    }
+    @Test
+    public void shouldGetAllPlantEmptyListOk(){
+        //given
+        given(plantRepository.findAll()).willReturn(List.of());
+
+        //when
+        List<PlantDto> result = plantService.getAll();
+
+        //then
+        then(plantRepository).should(times(1)).findAll();
+        assertEquals(0, result.size());
+    }
+    // GET ALL BY User tests
+    @Test
+    public void shouldGetAllPostsByUserOk() {
+        //given
+        given(plantRepository.findAll()).willReturn(List.of(plant));
+
+        //when
+        List<PlantDto> result = plantService.getAllByUser(plant.getUser().getId());
+
+        //then
+        then(plantRepository).should(times(1)).findAll();
+        assertEquals(1, result.size());
+        assertEquals(plant.getName(), result.get(0).getName());
+        assertEquals(plant.getPurchaseDate(), result.get(0).getPurchaseDate());
+        assertEquals(plant.getLocation(), result.get(0).getLocation());
+        assertEquals(category.getId(), result.get(0).getCategoryId());
+        assertEquals(species.getId(), result.get(0).getSpeciesId());
+        assertEquals(user.getId(), result.get(0).getUserId());
+
+    }
+    @Test
+    public void shouldGetAllPlantByUserEmptyListOk(){
+        //given
+        given(plantRepository.findAll()).willReturn(List.of(plant));
+
+        //when
+        List<PlantDto> result = plantService.getAllByUser(10L);
+
+        //then
+        then(plantRepository).should(times(1)).findAll();
+        assertEquals(0, result.size());
     }
 }
