f35651e2a8e52dd84d8642d59150c4facb9e9abb
Patrycja Zmilczak <patiz@vp.pl>
2021-10-10 19:15:30 +0200

    Add Swagger2 api info

--- src/main/java/com/hepiplant/backend/configuration/BackendConfiguration.java
+++ src/main/java/com/hepiplant/backend/configuration/BackendConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -28,7 +30,16 @@ public class BackendConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo(){
+        return new ApiInfo(
+                "Hepi Plant API",
+                "API for Hepi Plant application",
+                "1.0",
+                null, null, null, null, Collections.emptyList());
     }
 
 }
