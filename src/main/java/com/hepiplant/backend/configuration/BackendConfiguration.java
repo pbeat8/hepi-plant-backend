97d3b9a8811d9d1db89360158bc85d605109a586
Patrycja Zmilczak <patiz@vp.pl>
2021-10-10 19:08:29 +0200

    Add Swagger2

--- src/main/java/com/hepiplant/backend/configuration/BackendConfiguration.java
+++ src/main/java/com/hepiplant/backend/configuration/BackendConfiguration.java
@@ -2,18 +2,33 @@ package com.hepiplant.backend.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
 @Configuration
+@EnableSwagger2
 public class BackendConfiguration {
 
     @Bean
     public Validator validator(){
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         return factory.getValidator();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
