1d591591ad680c169bee826219b90081a7b0ca3d
Patrycja Zmilczak <patiz@vp.pl>
2021-10-26 16:27:47 +0200

    PZ: try to add datasource

--- src/main/java/com/hepiplant/backend/configuration/BackendConfiguration.java
+++ src/main/java/com/hepiplant/backend/configuration/BackendConfiguration.java
@@ -1,5 +1,8 @@
 package com.hepiplant.backend.configuration;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -9,6 +12,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.sql.DataSource;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
@@ -17,6 +21,16 @@ import java.util.Collections;
 @Configuration
 @EnableSwagger2
 public class BackendConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
 
     @Bean
     public Validator validator(){
