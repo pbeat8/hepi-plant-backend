43af9e610ee97a8bdeecd538e45f455d4b43177e
Patrycja Zmilczak <patiz@vp.pl>
2021-11-09 17:20:25 +0100

    PZ: add missing import

--- src/main/java/com/hepiplant/backend/service/impl/PlantServiceImpl.java
+++ src/main/java/com/hepiplant/backend/service/impl/PlantServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
