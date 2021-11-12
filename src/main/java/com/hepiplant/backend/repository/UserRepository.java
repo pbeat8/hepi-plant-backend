17aad8958dfbdea123ab8c6c68bd80df9e83f9ad
Patrycja Zmilczak <patiz@vp.pl>
2021-11-12 12:54:26 +0100

    PZ: add new user endpoints for admin

--- src/main/java/com/hepiplant/backend/repository/UserRepository.java
+++ src/main/java/com/hepiplant/backend/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.hepiplant.backend.repository;
 
 import com.hepiplant.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +12,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUid(String uId);
     Optional<User> findByEmail(String email);
+    @Query(value =
+            "SELECT count(*) FROM ( " +
+            "       SELECT * FROM forum.post_comments " +
+            "       WHERE user_id = ?1 " +
+            "       UNION " +
+            "       SELECT * FROM forum.sales_offer_comments " +
+            "       WHERE user_id = ?1) q ",
+    nativeQuery = true)
+    long getCommentsCountByUserId(Long id);
 }
